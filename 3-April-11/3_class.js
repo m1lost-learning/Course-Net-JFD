a032963c6d2c68b88ec99e0af3ae660e632490dc
m1lost <m1lost@live.co.id>
2026-04-11 14:30:37 +0700

    Incapsulate

--- 3-April-11/3_class.js
+++ 3-April-11/3_class.js
@@ -23,3 +23,44 @@ console.log(vehicle.type); // Output: Sedan
 console.log(vehicle.price); // Output: 20000
 vehicle.startEngine(); // Output: CIVIC Menyalakan Mesin.
 vehicle.stopEngine(); // Output: CIVIC Mematikan Mesin.
+
+//konsep OOP
+// 1. Encapsulation
+// 2. Inheritance
+// 3. Polymorphism
+// 4. Abstraction
+
+class Student {
+    #nama; // private field
+    gpa; // Not private field
+    
+    constructor(name, gpa) {
+        this.#nama = name;
+        this.gpa = gpa;
+    }
+
+    getName() {
+        return this.#nama;
+    }
+    getGpa() {
+        return this.gpa;
+    }
+
+    setName(name) {
+        this.#nama = name;
+    }
+    setGpa(gpa) {
+        this.gpa = gpa;
+    }
+}
+
+let student = new Student("Yuni", 3.9);
+console.log(student); // Output: Student { _name: 'Yuni', _gpa: 3.9 }
+console.log(student.getName()); // Output: Yuni
+console.log(student.getGpa()); // Output: 3.9
+
+student.setName("Yuni Setiawan");
+student.setGpa(3.8);
+console.log(student.getName()); // Output: Yuni Setiawan
+console.log(student.getGpa()); // Output: 3.8
+
