6909a15a877cc681542b0955cd6189178a4f6ca3
m1lost <m1lost@live.co.id>
2026-04-11 13:36:10 +0700

    Destructuring Sample

--- 3-April-11/2_destructuring.js
+++ 3-April-11/2_destructuring.js
@@ -6,10 +6,44 @@ const { name, age, city } = user;
 console.log(name); // Output: Joko
 console.log(age);  // Output: 28    
 console.log(city); // Output: Jakarta
+console.log(); 
 
-// make alias di destructuring
+// make alias di destructuring object
 const { name: userName, age: userAge, city: userCity } = user;
 
 console.log(userName); // Output: Joko
 console.log(userAge);  // Output: 28
 console.log(userCity); // Output: Jakarta
+console.log(); 
+
+// Destructuring juga dapat digunakan untuk array
+const colors = ['red', 'green', 'blue'];
+const [pertama, kedua, ketiga] =  colors;
+console.log(pertama); // Output: red
+console.log(kedua);   // Output: green
+console.log(ketiga);  // Output: blue
+console.log(); 
+
+
+// Destructuring fungction
+function greet({ name, age }) {
+    console.log(`Hello, my name is ${name}, usia saya ${age} tahun`);
+}
+
+greet({name: 'Joko S', age: 21}); // Output: Hello, my name is Joko S, usia saya 21 tahun
+
+// Destructruring default value dari object
+const person = { names: 'Joko'};
+const { names, hobby = 'Coding' } = person;
+console.log(person); // Output: { names: 'Joko' }
+console.log(names);  // Output: Joko
+console.log(hobby); // Output: Coding
+console.log();
+
+// destructuring nesting
+const pengguna = { nama: 'Joko', alamat: { kota: 'Jakarta', negara: 'Indonesia' } };
+const { nama, alamat: { kota, negara } } = pengguna;
+console.log(nama);         // Output: Joko
+console.log(kota);         // Output: Jakarta
+console.log(negara);       // Output: Indonesia
+
