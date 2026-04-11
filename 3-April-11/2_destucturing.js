7dcb0757d070ba11df0462b29d6a64e546006ab0
m1lost <m1lost@live.co.id>
2026-04-11 13:14:43 +0700

    Destuctuting

---
+++ 3-April-11/2_destucturing.js
@@ -0,0 +1,5 @@
+// Destructuring adalah fitur dalam JavaScript yang memungkinkan kita untuk mengambil nilai dari array atau properti dari objek dan menyimpannya dalam variabel dengan cara yang lebih singkat dan mudah dibaca.
+
+const user = { name: 'Joko', age: 28 };
+const { name, age } = user;
+
