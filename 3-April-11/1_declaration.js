99d50b4c49082cd3bc233ecd71f74346379d92d1
m1lost <m1lost@live.co.id>
2026-04-11 10:52:04 +0700

    declaterion

---
+++ 3-April-11/1_declaration.js
@@ -0,0 +1,28 @@
+
+const product= [
+    { 
+    name: "Laptop",
+    harga: 15000
+    },
+    {
+    name: "Maouse",
+    harga: 200
+    },
+    {
+    name: "Keyboard",
+    harga: 500
+    },
+    {
+    name: "monitor",
+    harga: 3000
+    }
+
+];
+ 
+const productFilter = product.filter(
+    function(item){ 
+        return item.harga > 1000;
+    }
+);
+
+console.log(productFilter);
