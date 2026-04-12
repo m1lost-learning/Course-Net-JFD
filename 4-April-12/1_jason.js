f3d0ad4b0baae9b112e0e1789f9039bbc031a788
m1lost <m1lost@live.co.id>
2026-04-12 10:33:29 +0700

    Test update

--- 4-April-12/1_jason.js
+++ 4-April-12/1_jason.js
@@ -3,4 +3,25 @@
 
 const fs = require('fs');
 let data = fs.readFileSync('data.json', 'utf-8');
+let dataobject = JSON.parse(data);
+console.log(dataobject);
+
+const databaru = [
+    {
+        nama : "joko dgdgdf",
+        alamat : "papua"
+    }
+];
+
+// konversi object javascript ke json
+const jsonData = JSON.stringify(databaru, null, 2);
+
+// write ke file data json
+fs.writeFileSync('data.json', jsonData, (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+    } else {
+        console.log('File written successfully');
+    }
+});
 
